9c09ca28d2d772d0f5b87f8c5d260fd15a81a6be
y.temp4 <y.temp4@gmail.com>
2015-08-22 23:17:49 +0900

    add page title and fix footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "kit-c | 金沢工業大学の非公式掲示板"
+    title = "kit-c"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{title}"
+    end
+  end
 end
