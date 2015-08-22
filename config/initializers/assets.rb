c09e681875b6ae6ec32d5aaa247d4ea9d2071432
y.temp4 <y.temp4@gmail.com>
2015-08-22 13:23:56 +0900

    fix design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( flatly.css )
 Rails.application.config.assets.precompile += %w( flatly.js )
+Rails.application.config.assets.precompile += %w( tops.css )
+Rails.application.config.assets.precompile += %w( posts.css )
